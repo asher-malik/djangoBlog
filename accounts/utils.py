26ffdd5cd8b672248a36ce5acb76291ca88fa382
Asher Malik <12345ashermalik@gmail.com>
2025-02-20 20:54:43 +0000

    Edit generate_token and confirm_token to be used once

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,10 +1,32 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+from itsdangerous import URLSafeTimedSerializer
 from django.conf import settings
+
+from .models import Token
+import os
+
+def generate_token(email):
+    serializer = URLSafeTimedSerializer(os.getenv('SECRET_KEY'))
+    token = serializer.dumps(email, salt='django-python')
+    token_data = Token.objects.create(token=token, email=email)
+    token_data.save()
+    return token
+
+def confirm_token(token, expiration=1800):
+    serializer = URLSafeTimedSerializer(os.getenv('SECRET_KEY'))
+    try:
+        email = serializer.loads(
+            token, salt='django-python', max_age=expiration
+        )
+        token_data = Token.objects.get(email=email, token=token)
+        if token_data.used:
+            return False
+        return email
+    except:
+        return False
+    
 
 def send_email(request, user, mail_subject, email_template):
     from_email = settings.DEFAULT_FROM_EMAIL
@@ -14,8 +36,7 @@ def send_email(request, user, mail_subject, email_template):
         'user': user,
         'protocal': 'https' if request.is_secure() else 'http',
         'domain': current_site,
-        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': default_token_generator.make_token(user),
+        'token': generate_token(user.email),
     })
     to_email = user.email
     mail = EmailMessage(mail_subject, message, to=[to_email], from_email=from_email)
